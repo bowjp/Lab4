e2a3c44f384b80a71dc6f61f6525b8e862fcbc80
Pete <petebowry@gmail.com>
2018-09-19 11:51:37 -0400

    reversed

--- Lab4.java
+++ Lab4.java
@@ -1,19 +1,29 @@
+
 import java.util.Arrays;
 
 public class Lab4 {
 public static void main(String args[]) {
 
-int[][] tests = new int[5][3];
+Integer[][] tests = new Integer[5][3];
 
-tests[0] = new int[]{1,2,3};
-tests[1] = new int[]{2,3,1};
-tests[2] = new int[]{3,2,1};
-tests[3] = new int[]{4,5,6};
-tests[4] = new int[]{5,7,1};
+tests[0] = new Integer[]{1,2,3};
+tests[1] = new Integer[]{2,3,1};
+tests[2] = new Integer[]{3,2,1};
+tests[3] = new Integer[]{4,5,6};
+tests[4] = new Integer[]{5,7,1};
 
-for (int[] i: tests) {
-	Arrays.sort(i);
-}; 
+for (Integer[] i: tests) {
+    ReverseSorter(i);
+}
 
-} 
+
+}
+    public static void ReverseSorter(Integer[] unsort){
+        Arrays.sort(unsort);
+        for(int j = 0; j < unsort.length/2; j++) {
+            int temp = unsort[j];
+            unsort[j] = unsort[unsort.length - j - 1];
+            unsort[unsort.length - j - 1] = temp;
+        }
+    }
 }
